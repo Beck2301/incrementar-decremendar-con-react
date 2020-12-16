317922019a590674ce1ca00cdfeff1dd46902586
Beck2301 <bryang211@hotmail.com>
2020-12-16 08:29:29 -0600

    2 files modified

--- src/App.js
+++ src/App.js
@@ -2,28 +2,39 @@ import React from "react";
 import { useState } from "react";
 
 function Contador() {
-  const [contadorValue, actualizarContador] = useState(76);
-  const [numeroDeVeces, actualizarVeces] = useState(0);
+  const [contadorValue, actualizarContador] = useState(174);
+  const [numeroDediscos, actualizarVeces] = useState(11);
 
   return (
-    <div>
-      <span>{contadorValue}</span>
+    <div className="container">
+      <h3>Álbumes: {contadorValue}</h3>
       <button
+        className="btn btn-primary m-2"
         onClick={() => {
           actualizarContador(contadorValue + 1);
-          actualizarVeces(numeroDeVeces + 1);
         }}
       >
         Incrementar +
       </button>
       <button
+        className="btn btn-danger"
         onClick={() => {
           actualizarContador(contadorValue - 1);
-          actualizarVeces(numeroDeVeces + 1);
         }}
       >
         Decrementar -{" "}
       </button>
+
+      <button
+        className="btn btn-warning m-2"
+        onClick={() => {
+          actualizarVeces(numeroDediscos + 1);
+        }}
+      >
+        Albúmes dobles++
+      </button>
+      <p> Albúm que tienen 2 o 3 discos: {numeroDediscos}</p>
+      <p> Total discos convertidos: {numeroDediscos + contadorValue}</p>
     </div>
   );
 }
